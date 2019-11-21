523a60b1ed4ac1ba5a0cb92a31a5aed76b9e2bab
Michael Lang <mi431lan@htwg-konstanz.de>
2019-11-21 16:43:33 +0100

    Renaming and moving

---
+++ app/src/main/java/mobile/computing/model/rest/RestTask.java
@@ -0,0 +1,23 @@
+package mobile.computing.model.rest;
+
+import com.ibm.cloud.sdk.core.security.IamAuthenticator;
+import com.ibm.watson.visual_recognition.v3.VisualRecognition;
+import com.ibm.watson.visual_recognition.v3.model.ClassifiedImages;
+import com.ibm.watson.visual_recognition.v3.model.ClassifyOptions;
+
+public class REST {
+
+    public void test(  ){
+        IamAuthenticator authenticator = new IamAuthenticator("xwlFoZ-SLxOmrIGmiirOIqCG6zOzyPKNHVnaccJWgwFD");
+        VisualRecognition visualRecognition = new VisualRecognition("2018-03-19", authenticator);
+        visualRecognition.setServiceUrl("https://gateway.watsonplatform.net/visual-recognition/api");
+
+        ClassifyOptions classifyOptions = new ClassifyOptions.Builder()
+                .url("https://ibm.biz/BdzLPG")
+                .build();
+        ClassifiedImages result = visualRecognition.classify(classifyOptions).execute().getResult();
+        System.out.println(
+                "\n******** Classify with the General model ********\n" + result
+                        + "\n******** END Classify with the General model ********\n");
+    }
+}
