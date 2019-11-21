ce28ae8cacc04dd7f21d1233f44e0f1c5589b7df
Michael Lang <mi431lan@htwg-konstanz.de>
2019-11-21 16:45:24 +0100

    Added method calls and button

--- app/src/main/java/mobile/computing/view/main/MainActivity.java
+++ app/src/main/java/mobile/computing/view/main/MainActivity.java
@@ -1,14 +1,30 @@
-package mobile.computing.project;
+package mobile.computing.view.main;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import mobile.computing.model.rest.RestTask;
+import mobile.computing.project.R;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    Button test;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        test = findViewById(R.id.test);
+        test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RestTask restTask = new RestTask();
+                restTask.execute();
+            }
+        });
     }
 }
