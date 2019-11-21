0afcf4a336c3db67f93c7c8f9c58594f3133660d
Michael Lang <mi431lan@htwg-konstanz.de>
2019-11-21 16:45:02 +0100

    Added method to call ibm watson api

--- app/src/main/java/mobile/computing/model/rest/RestTask.java
+++ app/src/main/java/mobile/computing/model/rest/RestTask.java
@@ -1,11 +1,20 @@
 package mobile.computing.model.rest;
 
+import android.os.AsyncTask;
+
 import com.ibm.cloud.sdk.core.security.IamAuthenticator;
 import com.ibm.watson.visual_recognition.v3.VisualRecognition;
 import com.ibm.watson.visual_recognition.v3.model.ClassifiedImages;
 import com.ibm.watson.visual_recognition.v3.model.ClassifyOptions;
 
-public class REST {
+public class RestTask extends AsyncTask<String,String,String> {
+
+    @Override
+    protected String doInBackground(String... strings) {
+        test();
+        return "a";
+
+    }
 
     public void test(  ){
         IamAuthenticator authenticator = new IamAuthenticator("xwlFoZ-SLxOmrIGmiirOIqCG6zOzyPKNHVnaccJWgwFD");
