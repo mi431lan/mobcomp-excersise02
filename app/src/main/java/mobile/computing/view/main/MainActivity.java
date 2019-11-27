d6064e904abcb47ef8c7c537f0e9352824930212
Michael Lang <mi431lan@htwg-konstanz.de>
2019-11-27 11:51:40 +0100

    Added textview, wired presenter and api task

--- app/src/main/java/mobile/computing/view/main/MainActivity.java
+++ app/src/main/java/mobile/computing/view/main/MainActivity.java
@@ -3,28 +3,55 @@ package mobile.computing.view.main;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-import mobile.computing.model.rest.RestTask;
+import org.w3c.dom.Text;
+
+import mobile.computing.presenter.rest.IBMWatsonAPITaskPresenter;
 import mobile.computing.project.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements IBMWatsonAPITaskPresenter.View {
 
+    private static final String TAG = "MainActivity";
+    IBMWatsonAPITaskPresenter ibmWatsonAPITaskPresenter;
 
-    Button test;
+    Button testButton;
+    TextView apiResult;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        test = findViewById(R.id.test);
-        test.setOnClickListener(new View.OnClickListener() {
+        ibmWatsonAPITaskPresenter = new IBMWatsonAPITaskPresenter(this);
+        apiResult = findViewById(R.id.apiResult);
+        apiResult.setMovementMethod(new ScrollingMovementMethod());
+        testButton = findViewById(R.id.testButton);
+        testButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                RestTask restTask = new RestTask();
-                restTask.execute();
+
+                Log.d(TAG, "clicked");
+                ibmWatsonAPITaskPresenter.classifyImage("https://ibm.biz/BdzLPG");
+
             }
         });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ibmWatsonAPITaskPresenter.stop();
+    }
+
+    @Override
+    public void updateUI(String result) {
+        apiResult.setText(result);
     }
 }
