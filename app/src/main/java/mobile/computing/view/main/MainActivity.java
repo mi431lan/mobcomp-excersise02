ed1ced0844c1e68831f32c9ca36b7f3da9951829
Michi <michi-lang08@web.de>
2019-12-05 12:54:58 +0100

    Added offline handling for api calls

--- app/src/main/java/mobile/computing/view/main/MainActivity.java
+++ app/src/main/java/mobile/computing/view/main/MainActivity.java
@@ -9,18 +9,26 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.treebo.internetavailabilitychecker.InternetAvailabilityChecker;
+import com.treebo.internetavailabilitychecker.InternetConnectivityListener;
+
 import org.w3c.dom.Text;
 
+import mobile.computing.persistence.sharedpref.SharedPreferencesManager;
 import mobile.computing.presenter.rest.IBMWatsonAPITaskPresenter;
 import mobile.computing.project.R;
 
-public class MainActivity extends AppCompatActivity implements IBMWatsonAPITaskPresenter.View {
+public class MainActivity extends AppCompatActivity implements IBMWatsonAPITaskPresenter.View, InternetConnectivityListener {
 
     private static final String TAG = "MainActivity";
-    IBMWatsonAPITaskPresenter ibmWatsonAPITaskPresenter;
+    private IBMWatsonAPITaskPresenter ibmWatsonAPITaskPresenter;
+    private InternetAvailabilityChecker mInternetAvailabilityChecker;
+    private SharedPreferencesManager sharedPreferencesManager;
 
-    Button testButton;
-    TextView apiResult;
+    private Button testButton;
+    private TextView apiResult;
+    private boolean hasInternet;
+    private String exampleImageUrl = "https://ibm.biz/BdzLPG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +36,14 @@ public class MainActivity extends AppCompatActivity implements IBMWatsonAPITaskP
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        InternetAvailabilityChecker.init(this);
+        mInternetAvailabilityChecker = InternetAvailabilityChecker.getInstance();
+        mInternetAvailabilityChecker.addInternetConnectivityListener(this);
+
         ibmWatsonAPITaskPresenter = new IBMWatsonAPITaskPresenter(this);
+
+        sharedPreferencesManager = new SharedPreferencesManager(this);
+
         apiResult = findViewById(R.id.apiResult);
         apiResult.setMovementMethod(new ScrollingMovementMethod());
         testButton = findViewById(R.id.testButton);
@@ -37,8 +52,14 @@ public class MainActivity extends AppCompatActivity implements IBMWatsonAPITaskP
             @Override
             public void onClick(View v) {
 
+                apiResult.setText("");
                 Log.d(TAG, "clicked");
-                ibmWatsonAPITaskPresenter.classifyImage("https://ibm.biz/BdzLPG");
+                if (hasInternet) {
+                    ibmWatsonAPITaskPresenter.classifyImage(exampleImageUrl);
+
+                } else {
+                    sharedPreferencesManager.saveString("url", exampleImageUrl);
+                }
 
             }
         });
@@ -48,10 +69,25 @@ public class MainActivity extends AppCompatActivity implements IBMWatsonAPITaskP
     protected void onDestroy() {
         super.onDestroy();
         ibmWatsonAPITaskPresenter.stop();
+        mInternetAvailabilityChecker.removeInternetConnectivityChangeListener(this);
+
     }
 
     @Override
     public void updateUI(String result) {
         apiResult.setText(result);
+    }
+
+    @Override
+    public void onInternetConnectivityChanged(boolean isConnected) {
+        hasInternet = isConnected;
+
+        if (!isConnected) {
+            Log.d(TAG, "no internet");
+        } else if (isConnected) {
+            Log.d(TAG, "internet");
+            ibmWatsonAPITaskPresenter.classifyImage(sharedPreferencesManager.readString("url"));
+
+        }
     }
 }
